7018239e9c899d1661cccb5ce244eb9373489cf6
Kellan Baker <kbaker8@binghamton.edu>
2018-09-29 19:09:53 -0400

    Changed index.js

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const serv = require('./server/server.js');
 const socketio = require('./server/socket.js');
 const Game = require('./game-logic/game.js');
-
-
-const s = new serv.Server('localhost',80);
-s.setRoutes();
-s.start();
+const DB = require('./database/db.js');
 
 ( async () => {
-	const db = await socketio.createDatabase('Tasks');
+	const s = new serv.Server('localhost',80);
+	s.start();
+
+	const db = new DB.DB('Tasks');
+	await db.login();
+
 	const socket =  socketio.createSocket(s.server);
-	
+
 	Game.setDatabase(db);
 	socketio.start(socket, db, Game.eventHandler);
-})()
-
-
-
-
-
-
+})();
