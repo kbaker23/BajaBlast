b73dc3c41740a4afa9007ab30e0fbf9ca765cbf1
Kellan Baker <kbaker8@binghamton.edu>
2018-09-29 22:23:52 -0400

    Better geo location

--- server/traffic.js
+++ server/traffic.js
@@ -1,13 +1,48 @@
-
+const geo = require('geoip-lite');
 const DATA = {
-    conn: []
+    conn: {}
 };
 
 module.exports.analyzer = function(socket){
-  const info = {
-    ip: socket.remoteAddress,
-    time: new Date()
-  };
-  DATA.conn.push(info);
-  console.log(DATA, '/n', socket._session.WebSocketReceiver);
+  const ip = socket.headers['x-forwarded-for'] || socket.remoteAddress;
+  const loc = geo.lookup(ip);
+  if(loc){
+    const city = loc.city;
+    const region = loc.region;
+    const country = loc.country;
+
+    if(conn[country]){
+      if(conn[country][region]){
+          if(conn[country][region][city]){
+            conn[country][region][city].push(new Date());
+          }
+          else{
+            conn[country][region][city] = {
+              time: [new Date()]
+            };
+          }
+      }
+      else{
+        conn[country][region] = {};
+        conn[country][region][city] = {
+          time: [new Date()]
+        };
+      }
+
+    }
+    else{
+      conn[country] = {};
+      conn[country][region] = {};
+      conn[country][region][city] = {
+        time: [new Date()]
+      };
+    }
+
+    console.log(conn);
+  }
+  else{
+    console.log('Cannot find ip');
+  }
+
+
 }
